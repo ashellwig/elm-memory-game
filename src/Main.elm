6689dcd5cd0d14cac48a738c7436a3b14532a325
Ashton Hellwig <hellwigashton@gmail.com>
2018-01-04 00:49:23 -0700

    turning to grunt

--- src/Main.elm
+++ src/Main.elm
@@ -18,8 +18,24 @@ main =
 -- TYPES
 
 
-type alias Model =
-    {}
+type alias Card =
+    { id : String
+    , group : Group
+    , flipped : Bool
+    }
+
+
+type Group
+    = A
+    | B
+
+
+type alias Deck =
+    List Card
+
+
+type Model
+    = Playing Deck
 
 
 type Msg
@@ -30,9 +46,43 @@ type Msg
 -- MODEL
 
 
+cards : List String
+cards =
+    [ "dinosaur"
+    , "8-ball"
+    , "baked-potato"
+    , "kronos"
+    , "rocket"
+    , "skinny-unicorn"
+    , "that-guy"
+    , "zeppelin"
+    ]
+
+
 createModel : ( Model, Cmd Msg )
 createModel =
-    ( {}, Cmd.none )
+    -- Our model now constists of the unshuffled deck
+    ( Playing deck, Cmd.none )
+
+
+initCard : Group -> String -> Card
+initCard group name =
+    { id = name
+    , group = group
+    , flipped = False
+    }
+
+
+deck : Deck
+deck =
+    let
+        groupA =
+            List.map (initCard A) cards
+
+        groupB =
+            List.map (initCard B) cards
+    in
+    List.concat [ groupA, groupB ]
 
 
 
@@ -48,23 +98,24 @@ update msg model =
 -- VIEW
 
 
-createCard : Html Msg
-createCard =
+cardClass : Card -> String
+cardClass card =
+    "card-" ++ card.id
+
+
+createCard : Card -> Html Msg
+createCard card =
     div [ class "container" ]
-        -- try: changing ("flipped", False) into ("flipped", True)
-        [ div [ classList [ ( "card", True ), ( "flipped", True ) ] ]
+        -- try changing ("flipped", False) into ("flipped", True)
+        [ div [ classList [ ( "card", True ), ( "flipped", False ) ] ]
             [ div [ class "card-back" ] []
-            , div [ class "front card-dinosaur" ] []
+            , div [ class ("front " ++ cardClass card) ] []
             ]
         ]
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "wrapper" ]
-        [ createCard
-        , createCard
-        , createCard
-        , createCard
-        , createCard
-        ]
+    case model of
+        Playing deck ->
+            div [ class "wrapper" ] (List.map createCard deck)
